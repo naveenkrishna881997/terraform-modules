d81e974f046ca116b0bece74b2eb2ae7c4f229e9
NaveenKumar Krishnamoorthy <naveen.krishna0809@gmail.com>
2025-03-21 15:56:04 +0530

    Update main.tf

--- network/main.tf
+++ network/main.tf
@@ -57,7 +57,7 @@ resource "aws_route_table" "public" {
     vpc_id = aws_vpc.my_vpc.id
 
     tags = {
-        Name = "DEMO Public RouteTable - WEZVATECH"
+        Name = "DEMO Public RouteTable - NAVEENKRISHNA"
     }
 }
 
